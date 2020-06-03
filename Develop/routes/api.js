67241426b82bdcdec7c671b0ad44459cfc1f173a
Haylie Goh <h.goh@deakin.edu.au>
2020-06-03 21:00:25 +1000

    POSTroutes

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -12,9 +12,31 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-//ADD a workout
+//UPDATE workout
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findOneAndUpdate(req.params.id, { $push: { exercises: req.body } })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+      console.log(dbWorkout);
+    })
+    .catch((err) => res.status(400).json(err));
+});
+
+//CREATE a workout
 router.post("/api/workouts", (req, res) => {
   Workout.create(req.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+//ADD a workout in range
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
