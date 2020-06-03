8d4942bad7fa562761ab4b4548dfe3fc2bdb6a1c
Haylie Goh <h.goh@deakin.edu.au>
2020-06-03 20:22:42 +1000

    fixed html routes

--- Develop/server.js
+++ Develop/server.js
@@ -15,8 +15,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false,
 });
 
-//api routes
+// routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
