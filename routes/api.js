e9f52716734357010fd3e13512ded4da995e461d
Haylie Goh <h.goh@deakin.edu.au>
2020-06-06 14:13:17 +1000

    update route amended

--- routes/api.js
+++ routes/api.js
@@ -14,7 +14,7 @@ router.get("/api/workouts", (req, res) => {
 
 //UPDATE workout
 router.put("/api/workouts/:id", (req, res) => {
-  Workout.findOneAndUpdate(req.params.id, { $push: { exercises: req.body } })
+  Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
     .then((dbWorkout) => {
       res.json(dbWorkout);
       console.log(dbWorkout);
